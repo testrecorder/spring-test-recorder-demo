77f2f3b501089ac7d42ef5468e9138a640412e75
Ionut Breaz <ibreaz@yahoo.com>
2021-06-17 09:41:07 +0300

    Initial commit

---
+++ src/main/java/com/onushi/springtestrecorder/demo/SpringTestRecorderDemoApplication.java
@@ -0,0 +1,13 @@
+package com.onushi.springtestrecorder.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestRecorderDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTestRecorderDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/onushi/springtestrecorder/demo/SpringTestRecorderDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onushi.springtestrecorder.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestRecorderDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
